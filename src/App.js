89089c0d918a341043b242354cd7140adf52367f
MiDrAjX <jeisommarques78@gmail.com>
2021-04-19 02:40:35 -0300

    style: colocando estilizações no projeto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react'
-import ListaDeNotas from './components/ListaDeNotas'
-import FormularioCadastro from './components/FomularioCadastro'
+import React, {Component} from 'react';
+import ListaDeNotas from './components/ListaDeNotas/ListaDeNotas';
+import FormularioCadastro from './components/FomularioCadastro/FomularioCadastro';
+import "./assets/App.css";
 
 class App extends Component {
   render(){
   return(
-    <>
+    <section className="conteudo">
     <FormularioCadastro/>
     <ListaDeNotas/>
-    </>
+    </section>
   )
   }
 }
